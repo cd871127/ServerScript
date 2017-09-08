5e823281679856ed25f3c7c9295aed3d929c1c8d
cdistc <cdistc@163.com>
2017-09-08 16:34:09 +0800

    1st

--- shell/deploy/CarbonMiao.sh
+++ shell/deploy/CarbonMiao.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#if [ ! -n "$1" ] ;then
-#    echo "useage: ./deploy.sh [project_name]"
-#	exit
-#fi
+if [ ! -n "$1" ] ;then
+    echo "useage: ./deploy.sh [project_name]"
+	exit
+fi
 
-#echo 1
 
-project_name=CarbonMiao
+project_name=$1
 build_dir=/home/cdistc/build/$project_name
 deploy_dir=/home/cdistc/app/$project_name
 
@@ -17,6 +16,13 @@ git pull origin master
 gradle clean
 gradle bootRepackage
 
-cp libs/*.jar $deploy_dir
+#shutdown the service
+
+#backup
+mv $deploy_dir/$project_name.jar $deploy_dir/$project_name.jar.111
+
+cp build/libs/*.jar $deploy_dir/$project_name.jar
+
+java -jar $project_name.jar >> $project_name.log > 2>&1 &
 
 
