387e40b0ac81e773513809277412210457b57127
cdistc <cdistc@163.com>
2017-09-08 17:06:51 +0800

    1st

--- shell/deploy/deploy_react.sh
+++ shell/deploy/deploy_react.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
+if [ ! -n "$1" ] ;then
+    echo "useage: ./deploy.sh [project_name]"
+	exit
+fi
+
+
+project_name=$1
+build_dir=/home/cdistc/build/$project_name
+deploy_dir=/home/cdistc/app/$project_name
+shell_dir=/home/cdistc/app/shell
+datetime=`date +%s`
+
+cd $build_dir
+git pull origin master
+
+rm -rf build/
+npm run build
+
+#shutdown the service
+$shell_dir/close_process.sh node
+
+#backup
+mv $deploy_dir/$project_name $deploy_dir/$project_name.$datetime
+
+cp build $deploy_dir/$project_name
+
+`serve -s $deploy_dir/$project_name >> $deploy_dir/$project_name.log 2>&1 &`
+
+
+
+
 
